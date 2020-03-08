076f6a0ad690dc0412a5087f5c801fe66bf8ce11
汪明 <1620315190@qq.com>
2020-03-09 00:15:36 +0800

    初始化《亿级流量电商详情页系统》

---
+++ src/main/java/com/roncoo/eshop/inventory/dao/impl/RedisDAOImpl.java
@@ -0,0 +1,43 @@
+package com.roncoo.eshop.inventory.dao.impl;
+
+import com.roncoo.eshop.inventory.dao.RedisDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+@Repository("redisDAO")
+public class RedisDAOImpl implements RedisDAO {
+
+//    @Resource
+////    private JedisCluster jedisCluster;
+////
+////    @Override
+////    public void set(String key, String value) {
+////        jedisCluster.set(key, value);
+////    }
+////
+////    @Override
+////    public String get(String key) {
+////        return jedisCluster.get(key);
+////    }
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void set(String key, String value) {
+        ValueOperations<String, String> stringOperations = stringRedisTemplate.opsForValue();
+        stringOperations.getAndSet(key, value);
+    }
+
+    @Override
+    public String get(String key) {
+        ValueOperations<String, String> stringOperations = stringRedisTemplate.opsForValue();
+        return stringOperations.get(key);
+    }
+}
